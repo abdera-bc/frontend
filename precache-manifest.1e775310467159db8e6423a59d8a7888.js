43de62ba4b6c9c611816f19850231dab9c87b1c1
moritzgvt <hello@moritzgut.de>
2022-04-06 11:17:42 +0100

    Release latest changes

---
+++ precache-manifest.1e775310467159db8e6423a59d8a7888.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42aa21ecfc83ec3a6e1be741ec731fe5",
+    "url": "/android-chrome-192x192.png"
+  },
+  {
+    "revision": "9204f4a20b1d6d7d514c70e76e3335e6",
+    "url": "/android-chrome-512x512.png"
+  },
+  {
+    "revision": "196b043cdfc63547a096feec02b2bb39",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "e4e40b0c82d228add33b5bcfe276a859",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "4157f51a4cf4b015fe3a",
+    "url": "/css/app.ec326645.css"
+  },
+  {
+    "revision": "fa8567d2802c752f1bfb",
+    "url": "/css/chunk-0ad3555e.9b16c910.css"
+  },
+  {
+    "revision": "47d7cd7e2ad374f5b1a4",
+    "url": "/css/chunk-0dbf735e.39dea4cd.css"
+  },
+  {
+    "revision": "9b5019e0a476e063b97c",
+    "url": "/css/chunk-3f873adf.a477bb6f.css"
+  },
+  {
+    "revision": "c112583b75a6383f75bb",
+    "url": "/css/chunk-7a61477e.a9112d1a.css"
+  },
+  {
+    "revision": "b9c01c49e916516cfa7248af1c1f946c",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "25272b540bd7fa8e72feb95e70a521d6",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "6f916ce8ada5d5facf5ad4e1266a486d",
+    "url": "/fonts/WorkSans-Regular.6f916ce8.ttf"
+  },
+  {
+    "revision": "9e1952c9d61a6cd14bf2162b85ee61df",
+    "url": "/fonts/WorkSans-SemiBold.9e1952c9.ttf"
+  },
+  {
+    "revision": "25a5fd2f4ed373d44f37b39c103537ee",
+    "url": "/img/abdera-helfen.25a5fd2f.jpg"
+  },
+  {
+    "revision": "3219241b3931c65e0d0ffd4ce3abb857",
+    "url": "/img/abdera-logo.3219241b.svg"
+  },
+  {
+    "revision": "31c75473061f9c8df9194bed1914b326",
+    "url": "/img/abdera-start.31c75473.jpg"
+  },
+  {
+    "revision": "0bfe93e5b5b5ca4ea5b8e779646ecf75",
+    "url": "/img/close-white.0bfe93e5.svg"
+  },
+  {
+    "revision": "d8874d856ee1134d2f6fb1be983bc527",
+    "url": "/img/close.d8874d85.svg"
+  },
+  {
+    "revision": "f96d2d6099dc75232236a7eab37f6731",
+    "url": "/img/document.f96d2d60.svg"
+  },
+  {
+    "revision": "adcf6a52f8c4948cf566116cf89eb421",
+    "url": "/img/error.adcf6a52.png"
+  },
+  {
+    "revision": "e862b5254336d7824f39da99cfcf1660",
+    "url": "/img/hamburger-white.e862b525.svg"
+  },
+  {
+    "revision": "bb1c88431dfb3d65765190771d1b5880",
+    "url": "/img/hamburger.bb1c8843.svg"
+  },
+  {
+    "revision": "47111b1db926b41b5213f2e9095e695d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4157f51a4cf4b015fe3a",
+    "url": "/js/app.1185522b.js"
+  },
+  {
+    "revision": "fa8567d2802c752f1bfb",
+    "url": "/js/chunk-0ad3555e.cd8119fd.js"
+  },
+  {
+    "revision": "47d7cd7e2ad374f5b1a4",
+    "url": "/js/chunk-0dbf735e.c82c6e6a.js"
+  },
+  {
+    "revision": "9b5019e0a476e063b97c",
+    "url": "/js/chunk-3f873adf.bc66da67.js"
+  },
+  {
+    "revision": "c112583b75a6383f75bb",
+    "url": "/js/chunk-7a61477e.1eed0408.js"
+  },
+  {
+    "revision": "202b578cacd71b1b9c9a",
+    "url": "/js/chunk-vendors.29b7b9fd.js"
+  },
+  {
+    "revision": "47d69349830b323079f55147db38b714",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "e0b41a9eb597ea22704880dbcce80290",
+    "url": "/maskable_icon.png"
+  },
+  {
+    "revision": "abecb826faeb66d65a1261ca624cd097",
+    "url": "/mstile-150x150.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c84188dbc6fffea998e0b773c0032478",
+    "url": "/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "5bfe3fdc4a531a11d1c2cfcdfa966abf",
+    "url": "/site.webmanifest"
+  }
+]);
