6a5f9739d221c2065e3ceaeee7f87593d45bd833
moritzgvt <hello@moritzgut.de>
2024-02-14 11:18:41 +0000

    Accept TBA options for entrrice fields

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+importScripts("/precache-manifest.f2fe6899d54e42a9efa41883e40723fe.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('abdera-data').then(function(cache) {
+      return cache.addAll(
+        [
+          '/',
+          '/index.html'
+        ]
+      );
+    })
+  );
+
+  console.log('Service worker installed.')
+});
+
+self.addEventListener('activate', function () {
+  self.clients.claim()
+
+  console.log('Control claimed');
+});
+
+self.addEventListener('fetch', function(event) {
+  if (!(event.request.url.indexOf('http') === 0)) {
+    return false;
+  }
+  event.respondWith(
+    caches.open('abdera-dynamic').then(function(cache) {
+      return fetch(event.request)
+        .then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        })
+        .catch(function() {
+            console.log('The site is served from offline cache.')
+
+            return caches.match(event.request);
+        })
+    })
+  );
+});
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
